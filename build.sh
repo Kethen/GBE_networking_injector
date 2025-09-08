b9c77059ba97ae942c0a2a4ebe5f425cc658704c
Katharine Chui <katharine.chui@gmail.com>
2025-09-08 16:52:53 +0200

    add github action and .gitignore, build changes

--- build.sh
+++ build.sh
@@ -6,10 +6,13 @@ mkdir release
 CC=i686-w64-mingw32-gcc
 $CC -g -fPIC -c log.c -o log.o -O0
 $CC -g -fPIC -c steamclient.c -o steamclient.o -Iminhook_1.3.3/include -O0
-$CC -g -shared -static -o steamclient64.dll steamclient.o log.o -Lminhook_1.3.3/bin -lMinHook.x86 -Wl,-Bstatic -lpthread
+$CC -g -shared -static -o release/steamclient.dll steamclient.o log.o -Lminhook_1.3.3/bin -lMinHook.x86 -Wl,-Bstatic -lpthread
 
 CC=x86_64-w64-mingw32-gcc
 $CC -g -fPIC -c log.c -o log.o -O0
 $CC -g -fPIC -c steamclient.c -o steamclient.o -Iminhook_1.3.3/include -O0
-$CC -g -shared -static -o steamclient64.dll steamclient.o log.o -Lminhook_1.3.3/bin -lMinHook.x64 -Wl,-Bstatic -lpthread
+$CC -g -shared -static -o release/steamclient64.dll steamclient.o log.o -Lminhook_1.3.3/bin -lMinHook.x64 -Wl,-Bstatic -lpthread
+
+cp minhook_1.3.3/bin/MinHook.x86.dll release/
+cp minhook_1.3.3/bin/MinHook.x64.dll release/
 
